9daf1f9260d983a41515ae62ac7803e6d2548fa0
Adnan Tariq <adnan6official@gmail.com>
2024-04-07 16:36:55 +0500

    learned about data types

---
+++ 02variables/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+//jwtToken := 1250;	    this gives an error because we can't decalre variable like this globally it work's in any method or in function
+
+func main() {
+	var name string = "adnan" //STRING DATA
+	fmt.Println(name)
+	fmt.Printf("The variable of type %T \n", name)
+
+	var isLogIn bool = false //BOOLEAN DATA
+	fmt.Println(isLogIn)
+	fmt.Printf("The variable of type %T  \n", isLogIn)
+
+	var numbers int = 1200 //BOOLEAN DATA
+	fmt.Println(numbers)
+	fmt.Printf("The variable of type %T \n", numbers)
+
+	var floatNumbers = 12.50 //BOOLEAN DATA
+	fmt.Println(floatNumbers)
+	fmt.Printf("The variable of type %T \n", floatNumbers)
+
+	// WITHOUT TYPE
+	const withoutType = "this is without type"
+	fmt.Println(withoutType)
+
+	//CREATE VARIABLE WITHOUT VAR KEWORD
+
+	withouVarKeyword := "this variable is declared without var keyword"
+	fmt.Println(withouVarKeyword)
+
+}
