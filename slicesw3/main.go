52cc251b2469786cbb0aa930ae3839977393f026
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2024-05-29 16:21:00 +0500

    again merging code with main

---
+++ slicesw3/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Creating slice using comon method
+	var mySlice = []string{"Adnan", "hanan"}
+	fmt.Println(mySlice, len(mySlice), cap(mySlice)) // len return the length of slice while cap return the capacity of slice
+
+	// Create a slice from array
+	var newArr = [4]int{11, 22, 33, 44}
+	var sliceFromNewArr = newArr[1:3]
+	fmt.Println(sliceFromNewArr)
+
+	// Create slice using make() function
+	makeSlice := make([]int, 1, 2)
+	fmt.Println(makeSlice)
+
+	days := "thursday"
+
+	switch days {
+	case "monday":
+		fmt.Println(false)
+	case "tuesday":
+		fmt.Println(false)
+	case "wednesday":
+		fmt.Println(false)
+	case "thursday":
+		fmt.Println(true)
+	default:
+		fmt.Println("there is no match")
+	}
+
+	/*
+		newNumber := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+		for i := 0; i < len(newNumber); i++ {
+			fmt.Println(8*i, "=", 8*i)
+		}
+	*/
+
+	fruit := []string{"Apple", "Banana", "Mango", "Orange", "Grapes"}
+
+	for _, value := range fruit {
+		if value == "Banana" {
+			continue
+		} else if value == "Orange" {
+			break
+		}
+
+		fmt.Println(value)
+	}
+
+}
