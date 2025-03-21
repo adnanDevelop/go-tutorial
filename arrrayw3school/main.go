401557952b37e17766a52eaf23c4588e2a58ba01
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2025-03-21 14:01:39 +0500

    learned about arrays

--- arrrayw3school/main.go
+++ arrrayw3school/main.go
@@ -2,29 +2,31 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	var students = [2]string{"Adnan", "Hanan"}
+// 	walrusStudends := [2]int{1, 2}
+// 	 fmt.Println(students, walrusStudends)
+
+// 	//  Access elements of an array
+// 	fmt.Println(students[0], walrusStudends[1])
+
+// 	// Changes second element value
+// 	walrusStudends[1] = 50
+// 	fmt.Println(walrusStudends)
+
+// 	/* Initiliaze only the second and third elements of the array*/
+// 	addNewElements := [5]int{1: 10, 4: 20}
+// 	addNewElements[3] = 30 	// adding new value
+
+// 	fmt.Println(addNewElements)
+// 	fmt.Println(addNewElements[1]) // get specific value using index number
+// 	fmt.Println(len(addNewElements)) //Checking the length of an array
+
+// }
+
 func main() {
-	/*
-		var students = [2]string{"Adnan", "Hanan"}
-		walrusStudends := [2]int{1, 2}
-		 fmt.Println(students, walrusStudends)
-
-
-		Access elements of an array
-		fmt.Println(students[0], walrusStudends[1])
-
-
-		Changes second element value
-		walrusStudends[1] = 50
-		fmt.Println(walrusStudends)
-
-		Initiliaze only the second and third elements of the array
-	*/
-
-	addNewElements := [5]int{1: 10, 4: 20}
-	addNewElements[3] = 30 	// adding new value
-
-	fmt.Println(addNewElements)
-	fmt.Println(addNewElements[1]) // get specific value using index number
-	fmt.Println(len(addNewElements)) //Checking the length of an array
-
+	var firstStrArr = [3]string{"first", "second", ""}
+	var firstInArr = [3]int{1, 2, 3}
+	firstStrArr[2] = "third";
+	fmt.Println(firstStrArr, "\n", firstInArr)
 }
