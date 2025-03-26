79af3963d835b0d7a8114fd06c9132e0c0ce6d50
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2025-03-26 15:55:20 +0500

    tested get and get by id api

--- api/controllers/user_controller.go
+++ api/controllers/user_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	"crud/models"
+	"crud/utils"
 	"net/http"
 	"time"
 
@@ -25,20 +26,34 @@ func GetUsers(c echo.Context) error {
 
 	cursor, err := userCollection.Find(ctx, bson.M{})
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
+		return c.JSON(http.StatusInternalServerError, utils.BadRequest{
+			Status:  http.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
+	defer cursor.Close(ctx)
+
 	var users []models.User
+
 	if err = cursor.All(ctx, &users); err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
+		return c.JSON(http.StatusInternalServerError, utils.BadRequest{
+			Status:  http.StatusInternalServerError,
+			Message: err.Error(),
+		})
 	}
-	return c.JSON(http.StatusOK, users)
+
+	return c.JSON(http.StatusOK, utils.Response{
+		Status:  http.StatusOK,
+		Message: "Success",
+		Data:    users,
+	})
 }
 
 func GetUserByID(c echo.Context) error {
 	id := c.Param("id")
 	objID, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, "Invalid ID format")
+		return c.JSON(http.StatusBadRequest, utils.BadRequest{Status: http.StatusBadRequest, Message: err.Error()})
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -47,7 +62,7 @@ func GetUserByID(c echo.Context) error {
 	var user models.User
 	err = userCollection.FindOne(ctx, bson.M{"_id": objID}).Decode(&user)
 	if err != nil {
-		return c.JSON(http.StatusNotFound, "User not found")
+		return c.JSON(http.StatusNotFound, utils.BadRequest{Status: http.StatusNotFound, Message: "User not found"})
 	}
 	return c.JSON(http.StatusOK, user)
 }
@@ -63,7 +78,7 @@ func CreateUser(c echo.Context) error {
 
 	result, err := userCollection.InsertOne(ctx, user)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
+		return c.JSON(http.StatusInternalServerError, utils.BadRequest{Status: http.StatusInternalServerError, Message: err.Error()})
 	}
 	user.ID = result.InsertedID.(primitive.ObjectID).Hex()
 	return c.JSON(http.StatusCreated, user)
@@ -86,7 +101,7 @@ func UpdateUser(c echo.Context) error {
 	update := bson.M{"$set": user}
 	_, err = userCollection.UpdateOne(ctx, bson.M{"_id": objID}, update)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
+		return c.JSON(http.StatusInternalServerError, utils.BadRequest{Status: http.StatusInternalServerError, Message: err.Error()})
 	}
 	return c.JSON(http.StatusOK, "User updated successfully")
 }
@@ -103,7 +118,7 @@ func DeleteUser(c echo.Context) error {
 
 	_, err = userCollection.DeleteOne(ctx, bson.M{"_id": objID})
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
+		return c.JSON(http.StatusInternalServerError, utils.BadRequest{Status: http.StatusInternalServerError, Message: err.Error()})
 	}
 	return c.JSON(http.StatusOK, "User deleted successfully")
 }
---
+++ api/utils/response.go
@@ -0,0 +1,19 @@
+package utils
+
+type Pagination struct {
+	CurrentPage int `json:"current_page"`
+	TotalPage   int `json:"total_page"`
+	TotalData   int `json:"total_data"`
+}
+
+type Response struct {
+	Status     int         `json:"status"`
+	Message    string      `json:"message"`
+	Data       interface{} `json:"data"` // Array of objects
+	Pagination *Pagination `json:"pagination"`
+}
+
+type BadRequest struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
